35e2fd530381bd82de2ef295b6805cd2bab89420
Jonathan Diaz <jonaalbert.adm@gmail.com>
2022-07-17 17:38:08 -0400

    #Imagen

--- INTEC/assets/Components/CarsList.js
+++ INTEC/assets/Components/CarsList.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Image } from 'react-native';
 import cedulaListStyle from '../styles/cedulaListStyle';
 
-class CarsList extends Component{
-    constructor(props){
+class CarsList extends Component {
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -13,38 +13,40 @@ class CarsList extends Component{
         };
     }
 
-    async getCars(){
-        try{
+    async getCars() {
+        try {
             const response = await fetch('https://localhost:7011/api/CarsTables');
             const json = await response.json();
-            this.setState({data:json});
+            this.setState({ data: json });
         }
-        catch(error){
-            console.log('Error API',error);
+        catch (error) {
+            console.log('Error API', error);
         }
-        finally{
-            this.setState({isLoading:false});
+        finally {
+            this.setState({ isLoading: false });
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getCars();
     }
 
-    render(){
-        const {data,isLoading} = this.state;
-        if(isLoading){
-            return(
+    render() {
+        const { data, isLoading } = this.state;
+        if (isLoading) {
+            return (
                 <View>
-                   <Text>Descargando lista</Text> 
+                    <Text>Descargando lista</Text>
                 </View>
             );
         }
 
-        return(
+        return (
+
             <View style={cedulaListStyle.viewMain}>
-                <FlatList data={data} keyExtractor={({id_cars},index) => id_cars}
-                    renderItem={({item}) => {
+                <FlatList data={data} keyExtractor={({ id_cars }, index) => id_cars}
+                    renderItem={({ item }) => {
+                        var imagenurl = item.imagen;
                         return <>
                             <View style={cedulaListStyle.viewList}>
                                 <Text>Codigo: {item.codigo}</Text>
@@ -54,7 +56,7 @@ class CarsList extends Component{
                                 <Text>Año: {item.año}</Text>
                                 <Text>Estado: {item.estado}</Text>
                                 <Text>Precio: {item.precio}</Text>
-                                <Text>Imagen: {item.imagen}</Text>
+                                <Image style={{ resizeMode: "contain", height: 100, width: 200, borderWidth: 1 }} source={{ uri: imagenurl }} />
                             </View>
                         </>
                     }} />
