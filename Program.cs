ae747d6b6a34c2a4d8f33e2695db88e528c79299
Jonathan Diaz <jonaalbert.adm@gmail.com>
2022-07-17 15:42:47 -0400

    Cors

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSqlServer<CatalogCarsContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("cors", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseHttpsRedirection();
 
