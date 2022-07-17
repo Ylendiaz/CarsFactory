3df3bb3c433c95c8ab5ff904873fa0d69332e2c9
Ylen Diaz <ylendiazpolanco@gmail.com>
2022-07-17 17:45:07 -0400

    Revert "Merge branch 'master' of https://github.com/Ylendiaz/CarsFactory"
    This reverts commit b44149ee00e19889d418f9dbaaf20701d2f67a91, reversing
    changes made to 84e422424db0fcd52628bf73fdf1a5f9a5640210.

--- INTEC/assets/Components/CarsList.js
+++ INTEC/assets/Components/CarsList.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
 import { Image, StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
 import cedulaListStyle from '../styles/cedulaListStyle';
 
@@ -8,13 +7,6 @@ const image = { uri: "https://i.stack.imgur.com/kOnzy.gif" };
 
 class CarsList extends Component{
     constructor(props){
-=======
-import { StyleSheet, Text, View, ActivityIndicator, FlatList, Image } from 'react-native';
-import cedulaListStyle from '../styles/cedulaListStyle';
-
-class CarsList extends Component {
-    constructor(props) {
->>>>>>> 35e2fd530381bd82de2ef295b6805cd2bab89420
         super(props);
 
         this.state = {
@@ -23,48 +15,42 @@ class CarsList extends Component {
         };
     }
 
-    async getCars() {
-        try {
+    async getCars(){
+        try{
             const response = await fetch('https://localhost:7011/api/CarsTables');
             const json = await response.json();
-            this.setState({ data: json });
+            this.setState({data:json});
         }
-        catch (error) {
-            console.log('Error API', error);
+        catch(error){
+            console.log('Error API',error);
         }
-        finally {
-            this.setState({ isLoading: false });
+        finally{
+            this.setState({isLoading:false});
         }
     }
 
-    componentDidMount() {
+    componentDidMount(){
         this.getCars();
     }
 
-    render() {
-        const { data, isLoading } = this.state;
-        if (isLoading) {
-            return (
+    render(){
+        const {data,isLoading} = this.state;
+        if(isLoading){
+            return(
                 <View>
-<<<<<<< HEAD
                 <View style={styles.container}>
                 <Image source={image} style = {{resizeMode:"cover", width: 100, height: 100, marginLeft: '36%'}}>
                 </Image>
                 </View>
-=======
-                    <Text>Descargando lista</Text>
->>>>>>> 35e2fd530381bd82de2ef295b6805cd2bab89420
                 </View>
             );
 
         }
 
-        return (
-
+        return(
             <View style={cedulaListStyle.viewMain}>
-                <FlatList data={data} keyExtractor={({ id_cars }, index) => id_cars}
-                    renderItem={({ item }) => {
-                        var imagenurl = item.imagen;
+                <FlatList data={data} keyExtractor={({id_cars},index) => id_cars}
+                    renderItem={({item}) => {
                         return <>
                             <View style={cedulaListStyle.viewList}>
                                 <Text>Codigo: {item.codigo}</Text>
@@ -74,7 +60,7 @@ class CarsList extends Component {
                                 <Text>Año: {item.año}</Text>
                                 <Text>Estado: {item.estado}</Text>
                                 <Text>Precio: {item.precio}</Text>
-                                <Image style={{ resizeMode: "contain", height: 100, width: 200, borderWidth: 1 }} source={{ uri: imagenurl }} />
+                                <Text>Imagen: {item.imagen}</Text>
                             </View>
                         </>
                     }} />
